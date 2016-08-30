0a8b30de5cb327e2f3c7301fc7da083d69f92c19
ssanida <ssanida@hotmail.com>
2016-08-30 15:44:47 +0300

    Fixed bugs

--- routes/public.js
+++ routes/public.js
@@ -3,13 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Mobile-Age Platform' });
+  res.render('index', { title: 'Mobile-Age Platform' });
 });
 
 /* GET signup page. */
-router.get('/signUp', function(req, res, next) {
-    res.render('signup', {
-    });
+router.get('/signUp/developer', function(req, res, next) {
+  res.render('signup', {
+    signUpAs: "developer"
+  });
+});
+
+router.get('/signUp/odp', function(req, res, next) {
+  res.render('signup', {
+    signUpAs: "odp"
+  });
 });
 
 
