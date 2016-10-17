f630e3736e00264aa7446c9c4c8d4eed3c40a39f
MichaelPap <papa.mich@hotmail.com>
2016-10-17 06:49:04 -0700

    Added route to support the series of database updates that occur on main procedures such as (instantiate application etc..)

---
+++ routes/dbactions.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var shell = require('shelljs');
+var request = require('request');
+
+// Create connection
+var dbcon = require('../models/dbconnect');
+
+// Access database
+var dbHandler = require('../models/dbHandler');
+
+// General configuration
+var config = require('../general_config');
+
+// Authentication
+var auth = require('../models/auth');
+
+
+router.post('/updateDB/initiateApp', function(req, res, next) {
+    
+    var container_id = req.body.container_id;
+    var image_tag = req.body.image_tag;
+    var app_id = req.body.app_id;
+    var repo_id = req.body.repo_id;
+    var port = req.body.port;
+    var user = req.body.user;
+    
+    var send = {};
+    
+    dbHandler.dbactions.create_rows(dbcon, 'containers', [['container_id', container_id], ['image_id', image_tag], ['developer_id', user], ['active', 1], ['port', port]], function(result){
+
+        if(result['queryStatus'] == 'Success'){
+
+            dbHandler.dbactions.update_table(dbcon, 'applications', [['container_id', container_id]], [['id', app_id, 0]], 1, function(result){
+
+                if(result['queryStatus'] == 'Success'){
+
+                    dbHandler.dbactions.create_rows(dbcon, 'app_repos', [['container_id', container_id], ['project_id', repo_id], ['developer', user]], function(result){
+
+                        if(result['queryStatus'] == 'Success'){
+
+                            send["routerStatus"] = "Success";
+                            send["routerMessage"] = "Database updated successfully";
+
+                            res.json(send);
+                        }
+                        else{
+                            send["routerStatus"] = "Failure";
+                            send["routerMessage"] = "DB query error - Create rows on app_repos";
+
+                            res.json(send);
+                        }
+                    });
+                }
+                else{
+                    send["routerStatus"] = "Failure";
+                    send["routerMessage"] = "DB query error - Update table applications";
+
+                    res.json(send);
+                }
+            });
+        }
+        else{
+
+            send["routerStatus"] = "Failure";
+            send["routerMessage"] = "DB query error - Create rows on containers";
+
+            res.json(send);
+        }
+    });
+});
+
+module.exports = router;
